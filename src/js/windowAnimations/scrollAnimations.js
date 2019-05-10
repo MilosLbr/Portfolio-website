984a0ed0b8810aa15964de8504d3b0e5ba09f95d
MilosLbr <brankovicmilos@hotmail.com>
2019-05-10 18:41:48 +0200

    finished about me section animations

--- src/js/windowAnimations/scrollAnimations.js
+++ src/js/windowAnimations/scrollAnimations.js
@@ -1,4 +1,6 @@
+const navbar = $('.navbar')[0];
 const skillsDivs = $('[data-animate = "skills"]');
+const aboutMeDivs = $('[data-animate = "aboutMe"]');
 
 
 const addAnimationsOnScroll = () => {
@@ -7,39 +9,71 @@ const addAnimationsOnScroll = () => {
         const windowHeight = $(window).height();  
         const windowWidth = $(window).width();   
         
-        // add delay to the second and third div on large scrreens
+        
         if(windowWidth > 751){
             
-            skillsDivs.each((index, elem) => {
-                let jqelem = $(elem);
-                let offset = jqelem.offset();
-                if(offset.top + 150 < scrollTop + windowHeight){
-                    if(index === 0){    
-                        jqelem.addClass('floatInBottom');
-                    }else if(index === 1 ){
-                        jqelem.addClass('floatInBottom delay05');
-                    }else if(index === 2){
-                        jqelem.addClass('floatInBottom delay1');
-                    }
-                    
-                }
-            });
+            animateSkillDivsLargeScreen(skillsDivs, scrollTop, windowHeight);
         }else{
-            // small screens, animate on scroll into view
             
-            skillsDivs.each((index, elem) => {
-                let jqelem = $(elem);
+            animateSkillDivsSmallScreen(skillsDivs, scrollTop, windowHeight);
             
-                let offset = jqelem.offset();
-    
-                if(offset.top + 150 < scrollTop + windowHeight){
-                    jqelem.removeClass('hide');
-                    jqelem.addClass('floatInBottom');
-                }
-            });
+        };
+        animateNavbar(navbar, scrollTop);
+        animateAboutMeDivs(aboutMeDivs, scrollTop, windowHeight);
+    });
+};
+
+const animateNavbar = (navbar, scrollTop) => {
+    if(scrollTop > 80){
+        $(navbar).css('background-color' , 'rgba(0, 0, 0, 0.65)')
+    }else{
+        $(navbar).css('background-color' , 'rgba(0, 0, 0, 0.3)')
+    }
+}
+
+const animateSkillDivsLargeScreen = (skillsDivs, scrollTop, windowHeight) => {
+    // add delay to the second and third div on large screens
+    skillsDivs.each((index, elem) => {
+        let jqelem = $(elem);
+        let offset = jqelem.offset();
+        if(offset.top + 150 < scrollTop + windowHeight){
+            if(index === 0){    
+                jqelem.addClass('floatInBottom');
+            }else if(index === 1 ){
+                jqelem.addClass('floatInBottom delay05');
+            }else if(index === 2){
+                jqelem.addClass('floatInBottom delay1');
+            }
+            
         }
     });
 };
+
+const animateSkillDivsSmallScreen = (skillsDivs, scrollTop, windowHeight) => {
+    // on small screens, animate on scroll into view
+    skillsDivs.each((index, elem) => {
+        let jqelem = $(elem);
+    
+        let offset = jqelem.offset();
+
+        if(offset.top + 150 < scrollTop + windowHeight){
+            jqelem.removeClass('hide');
+            jqelem.addClass('floatInBottom');
+        }
+    });
+}
+
+const animateAboutMeDivs = (aboutMeDivs, scrollTop, windowHeight) => {
+    aboutMeDivs.each((index, elem) => {
+        let jqelem = $(elem);
+        let offset = jqelem.offset();
+
+        if(offset.top + 150 < scrollTop + windowHeight){
+            
+            index === 0 ? jqelem.addClass('floatInLeft') : jqelem.addClass('floatInRight');
+        }
+    })
+}
 
 
 export{ addAnimationsOnScroll }
