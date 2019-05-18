4cd2c59ed75ebde00d14b745d543477d8c21ded9
MilosLbr <brankovicmilos@hotmail.com>
2019-05-19 00:21:31 +0200

    First build for first deploy

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,8 @@ const jqueryPlugin = new webpack.ProvidePlugin({
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlPlugin = new HtmlWebpackPlugin({
-    template: 'src/index.html'
+    template: 'src/index.html',
+    favicon: 'src/images/favicon.png'
 });
 
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
